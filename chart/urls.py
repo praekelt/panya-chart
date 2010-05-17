524282f720f5b1196b5baf72b79e15cac1792643
Jonathan Bydendyk <jonathan@qoda-macbook.local>
2010-05-17 12:45:52 +0200

    removed obsolete patterns

--- chart/urls.py
+++ chart/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'chart.views',
-    url(r'^list/$', 'object_list', name='chart_object_list'),
     url(r'^(?P<slug>[\w-]+)/$', 'object_detail', name='chart_object_detail'),
 )
