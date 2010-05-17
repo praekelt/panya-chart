2b044141eda0da574bbb0834d3fc2f8ba4d16788
Jonathan Bydendyk <jonathan@qoda-macbook.local>
2010-05-17 17:30:14 +0200

    added content generation script

--- chart/generator.py
+++ chart/generator.py
@@ -2,29 +2,34 @@ import random
 
 from generate import IMAGES
 from generate.json_loader import load_json
+from music.generator import generate as music_generator
 
-CHART_COUNT = 20
-CHART_ENTRY_COUNT = 100
+CHART_COUNT = 1
+CHART_ENTRY_COUNT = 40
 
 def generate():
     objects = []
     
     # gen chart entry objects
     for i in range(1, CHART_ENTRY_COUNT + 1):
+        
+        
+        if i > 20:
+            music_count = i - 20
+        else:
+            music_count = i
+        
         objects.append({
             "model": "chart.ChartEntry",
             "fields": {
-                "title": "Chart Entry %s Title" % i,
-                "state": "published",
-                "image": random.sample(IMAGES, 1)[0],
-                "previous_position": random.randint(0, 20),
-                "current_position": random.randint(0, 20),
-                "next_position": random.randint(0, 20),
+                "previous_position": random.randint(0, 40),
+                "current_position": i,
+                "next_position": random.randint(0, 40),
                 "remove": False,
                 "chart": {
                     "model": "chart.Chart",
                     "fields": {
-                        "title": "Chart %s Title" % random.randint(1, CHART_COUNT + 1),
+                        "title": "Chart 1 Title",
                         "state": "published",
                         "image": random.sample(IMAGES, 1)[0],
                         "sites": {
@@ -35,10 +40,10 @@ def generate():
                         },
                     }
                 },
-                "sites": {
-                    "model": "sites.Site",
-                    "fields": { 
-                        "name": "example.com"
+                "track": {
+                    "model": "music.Track",
+                    "fields": {
+                        "title": "Track %s Title" % music_count,
                     }
                 },
             },
