73375aa87ad5ea1e259d25ec42b2323d2e03c257
Jonathan Bydendyk <jonathan@qoda-macbook.local>
2010-05-17 17:29:53 +0200

    added chart admin classes

--- chart/admin.py
+++ chart/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from content.admin import ModelBaseAdmin
 from chart.models import Chart, ChartEntry, ChartOptions
 
-
-class ChartEntryAdmin(ModelBaseAdmin):
+'''
+class ChartEntryAdmin(admin.ModelAdmin):
     list_display = ModelBaseAdmin.list_display + ('chart',)
     list_filter = ModelBaseAdmin.list_filter + ('chart',)
-
+'''
 admin.site.register(Chart, ModelBaseAdmin)
-admin.site.register(ChartEntry, ChartEntryAdmin)
+admin.site.register(ChartEntry)
 admin.site.register(ChartOptions)
