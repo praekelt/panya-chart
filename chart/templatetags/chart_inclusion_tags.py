79fb08918031f9044728278727d869c184b7fe43
Jonathan Bydendyk <jonathan@qoda-macbook.local>
2010-05-17 12:45:34 +0200

    removed obsolete tags

--- chart/templatetags/chart_inclusion_tags.py
+++ chart/templatetags/chart_inclusion_tags.py
@@ -2,10 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('chart/inclusion_tags/chart_list.html')
-def chart_listing(object_list):
+@register.inclusion_tag('chart/inclusion_tags/chartentry_listing.html')
+def chartentry_listing(object_list):
     return {'object_list': object_list}
-
-@register.inclusion_tag('chart/inclusion_tags/chart_detail.html')
-def chart_detail(obj):
-    return {'object': obj}
