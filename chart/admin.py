15f464e98ad0b1095e01df6414340a186e5f1e9b
qoda <jonathan@blu-marble.co.za>
2010-05-18 14:14:54 +0200

    modified admin list and seark params

--- chart/admin.py
+++ chart/admin.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from content.admin import ModelBaseAdmin
 from chart.models import Chart, ChartEntry, ChartOptions
 
-'''
+
 class ChartEntryAdmin(admin.ModelAdmin):
-    list_display = ModelBaseAdmin.list_display + ('chart',)
-    list_filter = ModelBaseAdmin.list_filter + ('chart',)
-'''
+    list_display = ('chart', 'track', 'current_position', 'remove')
+    list_filter = ('chart', 'created')
+    search_fields = ('created',)
+
 admin.site.register(Chart, ModelBaseAdmin)
-admin.site.register(ChartEntry)
+admin.site.register(ChartEntry, ChartEntryAdmin)
 admin.site.register(ChartOptions)
