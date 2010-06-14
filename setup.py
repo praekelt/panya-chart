9bcb5929e9780b8b90395ed9c15444625c0b9819
shaunsephton <shaunsephton@gmail.com>
2010-06-14 12:32:25 +0200

    rename and lics

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-chart',
-    version='dev',
-    description='Django chart app.',
-    author='Praekelt Consulting',
+    name='panya-chart',
+    version='0.0.1',
+    description='Panya chart app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt International',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-chart',
+    license='BSD',
+    url='http://github.com/praekelt/panya-chart',
     packages = find_packages(),
+    install_requires = [
+        'panya',
+    ],
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Panya",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
 
