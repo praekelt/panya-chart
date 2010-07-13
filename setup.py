ef6ad28eaac5fabd0af1800b2e2aed390ffe6bcc
shaunsephton <shaunsephton@gmail.com>
2010-07-13 11:52:44 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panya-chart',
-    version='0.0.1',
+    version='0.0.2',
     description='Panya chart app.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt International',
